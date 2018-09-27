70ee9a2b23f7b7003297cd0f668a9cf530473da7
lainys <is-kirillka@yandex.ru>
2018-09-27 19:40:15 +0300

    lab1 changes

--- lab1/exs2.cu
+++ lab1/exs2.cu
@@ -1,18 +1,21 @@
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
 #include "device_launch_parameters.h"
-#include <stdio.h>
-#include <math.h>
+#include <iostream>
+#include <fstream>
+
+float check(int* a, int*b, int*c, int N, int k);
+float cudaParallel(int *a, int *b, int *c, int N);
 
 __global__ void kernel(int* a, int* b, int* c)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	
+
 	c[idx] = a[idx] + b[idx];
-	
+
 }
 
 template<typename T>
-T init(T a,int N) {
+T init(T a, int N) {
 
 	for (int i = 0; i < N; i++) {
 		a[i] = 1;
@@ -20,20 +23,64 @@ T init(T a,int N) {
 	return a;
 }
 
-int main(int argc, char *argv[])
-{
-	int N = 10;
 
-	int* a = (int*) malloc(N * sizeof(int));
-	int* b = (int*) malloc(N * sizeof(int));
-	int* c = (int*) malloc(N * sizeof(int));
+int main()
+{
+	int k = 7;
+
+	float *ans1 = new float[k];
+	int *var_N = new int[k];
+
+	for (int i = 0, N = 10; i < k; i++, N *= 10) {
+		var_N[i] = N;
+
+		int *a = new int[N];
+		int *b = new int[N];
+		int *c = new int[N];
+
+		init(a, N);
+		init(b, N);
+
+		ans1[i] = check(a, b, c, N, 100);
+
+
+		delete a, b, c;
+
+		std::cout << i << std::endl;
+	}
+
+	std::ofstream out("text2.txt");
+
+	for (int i = 0; i < k; i++) {
+		out << var_N[i] << " ";
+	}
+	out << std::endl;
+
+	for (int i = 0; i < k; i++) {
+		out << ans1[i] << " ";
+	}
+	out << std::endl;
+
+	delete ans1, var_N;
+	return 0;
+}
+
+float check(int* a, int*b, int*c, int N, int k) {
+	float res = 0;
+
+	for (int i = 0; i < k; i++) {
+		res += cudaParallel(a, b, c, N);
+	}
+
+	return res / (k * 1000.0f);
+}
+
+
+float cudaParallel(int *a, int *b, int *c, int N) {
 
 	int* dA = nullptr;
 	int* dB = nullptr;
 	int* dC = nullptr;
-
-	a = init(a, N);
-	b = init(b, N);
 
 	cudaMalloc((void**)&dA, N * sizeof(int));
 	cudaMalloc((void**)&dB, N * sizeof(int));
@@ -41,8 +88,8 @@ int main(int argc, char *argv[])
 
 	cudaMemcpy(dA, a, N * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dB, b, N * sizeof(int), cudaMemcpyHostToDevice);
-	
-	cudaEvent_t start, stop;		
+
+	cudaEvent_t start, stop;
 	float gpuTime = 0.0f;
 
 	cudaEventCreate(&start);
@@ -50,15 +97,15 @@ int main(int argc, char *argv[])
 
 	cudaEventRecord(start, 0);
 
-	kernel <<<dim3(N/10,1), dim3(10,1)>>> (dA,dB,dC);
+	kernel << <dim3(N / 10, 1), dim3(10, 1) >> > (dA, dB, dC);
 
 	cudaEventRecord(stop, 0);
 
 	cudaEventSynchronize(stop);
 
 	cudaEventElapsedTime(&gpuTime, start, stop);
-	printf("time spent executing by the GPU: %.5f ms\n", gpuTime);
-	
+	//printf("time spent executing by the GPU: %.5f ms\n", gpuTime);
+
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
 
@@ -68,13 +115,5 @@ int main(int argc, char *argv[])
 	cudaFree(dC);
 
 
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", c[i]);
-	}
-
-	free(a);
-	free(b);
-	free(c);
-
-	return 0;
+	return gpuTime;
 }
--- lab1/exs3.cu
+++ lab1/exs3.cu
@@ -1,10 +1,17 @@
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/functional.h> 
 
-#include <iostream> 
-#include <cstdlib> 
-#include <ctime> 
+#include <iostream>
+#include <fstream>
+
+float check(int* a, int*b, int*c, int N, int k);
+float cudaParallel(int *a, int *b, int *c, int N);
+
+float checkThrust(int* a, int*b, int*c, int N, int k);
+float cudaParallelThrust(int *a, int *b, int *c, int N);
 
 struct sum_functor
 {
@@ -21,39 +28,170 @@ thrust::device_vector<int> multi_fast(thrust::device_vector < int > & X, thrust:
 	return Y;
 }
 
-int main(int argc, char* argv[])
+__global__ void kernel(int* a, int* b, int* c)
 {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	for (int i = 10; i < 100000000; i *= 10) {
-		thrust::device_vector<int> a(i);
-		thrust::device_vector<int> b(i);
+	c[idx] = a[idx] + b[idx];
 
-		thrust::fill(a.begin(), a.end(), 3);
-		thrust::fill(b.begin(), b.end(), 1);
+}
 
+template<typename T>
+T init(T a, int N) {
 
-		cudaEvent_t start, stop;
-		float gpuTime = 0.0f;
-
-		cudaEventCreate(&start);
-		cudaEventCreate(&stop);
-
-		cudaEventRecord(start, 0);
-
-		multi_fast(a, b);
-
-		cudaEventRecord(stop, 0);
-
-		cudaEventSynchronize(stop);
-
-		cudaEventElapsedTime(&gpuTime, start, stop);
-		std::cout<<gpuTime<<std::endl;
-		
-		
-
-
-		//for (int i = 0; i < b.size(); i++)
-		//	std::cout << "b[" << i << "] = " << b[i] << std::endl;
+	for (int i = 0; i < N; i++) {
+		a[i] = 1;
 	}
+	return a;
+}
+
+
+int main()
+{
+	int k = 7;
+
+	float *ans1 = new float[k];
+	float *ans2 = new float[k];
+	int *var_N = new int[k];
+
+	for (int i = 0, N = 10; i < k; i++, N *= 10) {
+		var_N[i] = N;
+
+		int *a = new int[N];
+		int *b = new int[N];
+		int *c = new int[N];
+
+		init(a, N);
+		init(b, N);
+
+		ans1[i] = check(a, b, c, N, 100);
+		ans2[i] = check(a, b, c, N, 100);
+
+
+		delete a, b, c;
+
+		std::cout << i << std::endl;
+	}
+
+	std::ofstream out("text3.txt");
+
+	for (int i = 0; i < k; i++) {
+		out << var_N[i] << " ";
+	}
+	out << std::endl;
+
+	for (int i = 0; i < k; i++) {
+		out << ans1[i] << " ";
+	}
+	out << std::endl;
+
+	for (int i = 0; i < k; i++) {
+		out << ans2[i] << " ";
+	}
+	out << std::endl;
+
+	delete ans1,ans2, var_N;
 	return 0;
 }
+
+float check(int* a, int*b, int*c, int N, int k) {
+	float res = 0;
+
+	for (int i = 0; i < k; i++) {
+		res += cudaParallel(a, b, c, N);
+	}
+
+	return res / (k * 1000.0f);
+}
+
+
+float checkThrust(int* a, int*b, int*c, int N, int k) {
+	float res = 0;
+
+	for (int i = 0; i < k; i++) {
+		res += cudaParallelThrust(a, b, c, N);
+	}
+
+	return res / (k * 1000.0f);
+}
+
+
+float cudaParallel(int *a, int *b, int *c, int N) {
+
+	int* dA = nullptr;
+	int* dB = nullptr;
+	int* dC = nullptr;
+
+	cudaMalloc((void**)&dA, N * sizeof(int));
+	cudaMalloc((void**)&dB, N * sizeof(int));
+	cudaMalloc((void**)&dC, N * sizeof(int));
+
+	cudaMemcpy(dA, a, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dB, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
+	cudaEvent_t start, stop;
+	float gpuTime = 0.0f;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start, 0);
+
+	kernel << <dim3(N / 10, 1), dim3(10, 1) >> > (dA, dB, dC);
+
+	cudaEventRecord(stop, 0);
+
+	cudaEventSynchronize(stop);
+
+	cudaEventElapsedTime(&gpuTime, start, stop);
+	//printf("time spent executing by the GPU: %.5f ms\n", gpuTime);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	cudaMemcpy(c, dC, N * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(dA);
+	cudaFree(dB);
+	cudaFree(dC);
+
+
+	return gpuTime;
+}
+
+float cudaParallelThrust(int *a, int *b, int *c, int N) {
+	
+	thrust::device_vector<int> at(N);
+	thrust::device_vector<int> bt(N);
+
+	for (int i = 0; i < N; i++) {
+		at[i] = a[i];
+		bt[i] = b[i];
+	}
+
+	cudaEvent_t start, stop;
+	float gpuTime = 0.0f;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start, 0);
+
+	multi_fast(at, bt);
+
+	cudaEventRecord(stop, 0);
+
+	cudaEventSynchronize(stop);
+
+	cudaEventElapsedTime(&gpuTime, start, stop);
+	//printf("time spent executing by the GPU: %.5f ms\n", gpuTime);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+	
+	for (int i = 0; i < N; i++) {
+		c[i] = b[i];
+	}
+
+	return gpuTime;
+}
+
